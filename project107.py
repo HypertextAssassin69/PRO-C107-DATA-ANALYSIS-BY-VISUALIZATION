b7c07b80b27c7bb725a22ed06acb7ce3069e014b
HypertextAssassin <72544486+HypertextAssassin69@users.noreply.github.com>
2022-06-08 17:26:02 +0530

    Update project107.py

--- project107.py
+++ project107.py
@@ -4,5 +4,7 @@ import plotly.express as px
 df = pd.read_csv("https://raw.githubusercontent.com/whitehatjr/Data-Analysis-by-visualisation/master/data.csv")
 mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
 
+#size means more the attempt bigger will be the circle
+#color also will be set by the average of attempts
 fig = px.scatter(mean, x="student_id", y="level", color="attempt", size="attempt")
 fig.show()
